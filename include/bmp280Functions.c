103335082b38973a6e0554cbe997062f749ca201
Gabe Perez <gperez58857@gmail.com>
2026-09-02 22:12:03 -0700

    started writing the pressure function and changes form to match bosch's

--- include/bmp280Functions.c
+++ include/bmp280Functions.c
@@ -19,7 +19,6 @@ int16_t dig_Cn(uint8_t MSB_RWA, uint8_t LSB_RWA){
 //algo to read the temp via data sheet by bosch
 int32_t temp(uint16_t dig_T1, int16_t dig_T2, int16_t dig_T3){ 
     SPI_WriteVal(0xF4, 0x21);//put bmp-280 into forced mode
-    delay(10);
 
     uint8_t temp_XLSB_Val = SPI_ReadVal(0xFC);//read value for XLSB register for temp 
     uint8_t temp_LSB_Val = SPI_ReadVal(0xFB);//read value for LSB register for temp 
@@ -27,9 +26,44 @@ int32_t temp(uint16_t dig_T1, int16_t dig_T2, int16_t dig_T3){
     
     int32_t U = (temp_MSB_Val << 12) | (temp_LSB_Val << 4) | (temp_XLSB_Val >> 4); 
 
-    int32_t x = U / 8 - (int32_t)dig_T1 * 2;//scaling such that can still get ints          
-    int32_t var_1 = x * (int32_t)dig_T2 / 2048;
-    int32_t var_2 = ((x/2) * ((x/2) / 4096)) * (int32_t)dig_T3 / 16384;
+    double var_1 = ((double)U / 16384.0 - (double)dig_T1 / 1024.0)*(double)dig_T2; 
+    double var_2 = ((double)U / 131072.0 - (double)dig_T1 / 8192.0)*((double)U / 131072.0 - (double)dig_T1 / 8192.0)*(double)dig_T3;
+ 
+    return (((var_1 + var_2) / 5120.0) * 100);
+} 
 
-    return ((var_1 + var_2) * 5 + 128) >> 8;
+int32_t pres(uint16_t dig_P1, int16_t dig_P2, int16_t dig_P3, int16_t dig_P4, int16_t dig_P5, int16_t dig_P6, int16_t dig_P7, int16_t dig_P8, int16_t dig_P9, uint16_t dig_T1, int16_t dig_T2, int16_t dig_T3){
+    SPI_WriteVal(0xF4, 0x05); 
+
+    uint8_t pres_XLSB_Val = SPI_ReadVal(0xF9);
+    uint8_t pres_LSB_Val = SPI_ReadVal(0xF8);
+    uint8_t pres_MSB_Val = SPI_ReadVal(0xF7);
+
+    int32_t U = (pres_MSB_Val << 12) | (pres_LSB_Val << 4) | (pres_XLSB_Val >> 4);
+
+    double var_1T = ((double)U / 16384.0 - (double)dig_T1 / 1024.0)*(double)dig_T2; 
+    double var_2T = ((double)U / 131072.0 - (double)dig_T1 / 8192.0)*((double)U / 131072.0 - (double)dig_T1 / 8192.0)*(double)dig_T3;
+
+
+    double var_1 = (var_1T+var_2T / 2.0)*64000.0;
+    double var_2 = var_1*var_1 * ((double)dig_P6/32768.0); 
+
+    var_2 = var_2 + var_1 * (double)dig_P5 *2.0; 
+    var_2 = (var_2/4.0)*(double)dig_P4*65536.0;  
+    
+    var_1 = (double)dig_P3 * var_1 * (var_1 / 524288.0) + (((double)dig_P2 * var_1) / 524288.0);
+    var_1 = (1 + var_1/32768.0)*(double)dig_P1; 
+
+    double p = 1048576.0 - (double)U;
+    p = (p - (var_2 / 4096.0)) * (6250.0 / var_1);
+    
+    var_1 = (double)dig_P9 * p * (p/2147483648.0); 
+    var_2 = p * ((double)dig_P8 / 32768.0); 
+
+    p = p + (var_1 + var_2 + (double)dig_P7) / 16.0;
+
+    return p; 
 }
+
+
+
--- include/bmp280Functions.h
+++ include/bmp280Functions.h
@@ -12,3 +12,4 @@ int16_t dig_Cn(uint8_t MSB_RWA, uint8_t LSB_RWA);
 
 //temp and pres
 int32_t temp(uint16_t dig_T1, int16_t dig_T2, int16_t dig_T3);
+int32_t pres(uint16_t dig_P1, int16_t dig_P2, int16_t dig_P3, int16_t dig_P4, int16_t dig_P5, int16_t dig_P6, int16_t dig_P7, int16_t dig_P8, int16_t dig_P9, uint16_t dig_T1, int16_t dig_T2, int16_t dig_T3);
